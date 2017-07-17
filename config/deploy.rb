72e6d1aad9a22d0f9f67382688d478e8a5ed12c9
Narachii <naoya706oxford@gmail.com>
2017-07-17 14:47:57 +0900

    Modified Cap ver 8

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.3.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/NaoyaNara/.ssh/naoya7063.pem']
 
-set :unicorn_pid, -> { "var/www/Airbnb/shared/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
