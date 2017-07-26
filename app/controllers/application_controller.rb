d139850bc7c0f97eab3ade4193d16f75b9ab0bd3
Narachii <naoya706oxford@gmail.com>
2017-07-26 20:09:51 +0900

    Edit application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true, with: :exception
+  skip_before_filter :verify_authenticity_token, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name,:last_name,:birthday_month,:birthday_day,:birthday_year])
+  end
 end
