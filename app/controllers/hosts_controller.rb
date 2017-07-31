5e6ec57432235e7243545a941deb09c605125005
Narachii <naoya706oxford@gmail.com>
2017-07-31 22:55:24 +0900

    made view for hosts controller

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -3,9 +3,15 @@ class HostsController < ApplicationController
 	def index
 	end
 
-	def new
+	def show
 	end
 
 	def create
+		host = Host.create(create_params)
+		redirect_to controller: 'rooms', action: 'index'
+	end
+
+	def create_params
+		params.permit(:user_id)
 	end
 end
---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,45 @@
+class RoomsController < ApplicationController
+
+	def index
+	end
+
+	def room
+	end
+
+	def bedrooms
+	end
+
+	def bed_details
+	end
+
+	def bathrooms
+	end
+
+	def locations
+		@room = Room.new
+	end
+
+	def amenities
+		@room = Room.new
+	end
+
+	def spaces
+		@room = Room.new
+	end
+
+	def image
+		@room = Room.new
+	end
+
+	def description
+		@room = Room.new
+	end
+
+	def title
+		@room = Room.new
+	end
+
+	def phone
+		@room= Room.new
+	end
+end
