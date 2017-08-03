a3c56e6f3610088bf2adf1bb85740d419999551f
Narachii <naoya706oxford@gmail.com>
2017-08-03 16:58:46 +0900

    Add models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
 	belongs_to :host
+	has_many :amenity_rooms
+	has_many :space_rooms
+	has_many :amenities, through: :amenity_rooms
+	has_many :spaces, through: :space_rooms
 	mount_uploader :image, ImageUploader
 end
---
+++ app/models/space_room.rb
@@ -0,0 +1,4 @@
+class SpaceRoom < ApplicationRecord
+  belongs_to :room
+  belongs_to :space
+end
