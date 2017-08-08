dcd775987164cd36fe11044c7f785425a4385358
Narachii <naoya706oxford@gmail.com>
2017-08-08 14:54:35 +0900

    Modified application.js(2)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 //= require underscore
 //= require gmaps/google
+//= require turbolinks
+//= require_tree .
