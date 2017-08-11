8c6734ca3dc7e0cdb8289c072a9ae3fbb099436f
Narachii <naoya706oxford@gmail.com>
2017-08-11 12:14:54 +0900

    Modified production.rb(2)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'http://52.199.245.236:3000/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:               'smtp.gmail.com',
