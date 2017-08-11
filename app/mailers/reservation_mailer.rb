25d8fb8ae1a78a532a5d1436f978610fd83088f7
Narachii <naoya706oxford@gmail.com>
2017-08-11 11:49:48 +0900

    Created email for reservation request

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -10,4 +10,14 @@ class ReservationMailer < ApplicationMailer
 	  	@check_out = reservation.date_out
     	mail to: user.email, subject: "Confirmation of your reservation request"
   end
+
+  def post_email_to_host(user,reservation,room,host)
+  		@user = user
+	  	@host_name = host.user.first_name
+	  	@title = room.name
+	  	@location = room.city
+	  	@check_in = reservation.data_in
+	  	@check_out = reservation.date_out
+  		mail to: host.user.email, subject: "Reservation request from #{@check_in} to #{@check_out}"
+  end
 end
