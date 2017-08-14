14950084950ced5be3c59f5721110d07bdc331bc
Narachii <naoya706oxford@gmail.com>
2017-08-14 19:47:51 +0900

    Changed DataBase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809035211) do
+ActiveRecord::Schema.define(version: 20170814102536) do
 
   create_table "amenities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(version: 20170809035211) do
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
     t.boolean  "progress",                     default: false, null: false
+    t.boolean  "progress_1",                   default: false, null: false
+    t.boolean  "progress_2",                   default: false, null: false
+    t.integer  "price"
   end
 
   create_table "space_rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
