c1ccc4cb35bb4af188c65ff9bcd03195df4583ec
Narachii <naoya706oxford@gmail.com>
2017-08-25 17:28:31 +0900

    associated reservations and message

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,6 +4,7 @@ class Room < ApplicationRecord
 	has_many :space_rooms
 	has_many :amenities, through: :amenity_rooms
 	has_many :spaces, through: :space_rooms
+	has_many :reservations
 	mount_uploader :image, ImageUploader
 	geocoded_by :address, :latitude  => :lattitude, :longitude => :longitude
 	after_validation :geocode, if: :city_changed?
